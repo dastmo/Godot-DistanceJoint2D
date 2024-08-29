297373dc1cff7e0285d8cd25f79c44d728f75880
Damyan Momchev <dastmo@gmail.com>
2024-08-29 10:08:15 +0300

    Fixed the majority of chaining issues

--- distance_joint_2d.gd
+++ distance_joint_2d.gd
@@ -3,13 +3,13 @@ class_name DistanceJoint2D
 
 
 @export var disable_collision: bool = true
-@export var anchor: NodePath = NodePath("")
+@export var pivot: NodePath = NodePath("")
 @export var links: Array[RigidBody2D]
 @export var uniform_distance: bool = false
 @export var max_distance: float = 0.0
 
 
-var _anchor: Node2D
+var _pivot: Node2D
 var _joint_valid: bool = true
 var _link_distances: Array[float] = []
 
@@ -18,8 +18,8 @@ func _ready() -> void:
 
 
 func init_joint() -> void:
-	if anchor != NodePath(""):
-		_anchor = get_node(anchor)
+	if pivot != NodePath(""):
+		_pivot = get_node(pivot)
 	_joint_valid = _validate_joint()
 	
 	if not _joint_valid:
@@ -41,22 +41,22 @@ func _validate_joint() -> bool:
 		push_error("No Rigidbody2D added to the links array. Joint is invalid.")
 		return false
 	
-	if links.size() < 2 and not is_instance_valid(_anchor):
+	if links.size() < 2 and not is_instance_valid(_pivot):
 		push_error("Add at least two links or one link and one pivot. Joint is invalid.")
 		return false
 	
-	if _anchor is RigidBody2D:
-		if _anchor.freeze == false:
-			_anchor.freeze = true
+	if _pivot is RigidBody2D:
+		if _pivot.freeze == false:
+			_pivot.freeze = true
 			push_warning("Rigidbody2D static pivot frozen by joint.")
 	
 	return true
 
 
 func _add_collision_exceptions() -> void:
-	if _anchor is PhysicsBody2D:
+	if _pivot is PhysicsBody2D:
 		for link in links:
-			_anchor.add_collision_exception_with(link)
+			_pivot.add_collision_exception_with(link)
 	
 	for link in links:
 		for other_link in links:
@@ -74,7 +74,7 @@ func _set_initial_distances() -> void:
 
 
 func _set_uniform_distances() -> void:
-	if is_instance_valid(_anchor):
+	if is_instance_valid(_pivot):
 		var _links_distance: float = max_distance / links.size()
 		for i in links.size():
 			_link_distances.append(_links_distance)
@@ -90,8 +90,8 @@ func _set_uniform_distances() -> void:
 func _set_distances() -> void:
 	for i in links.size():
 		if i == 0:
-			if is_instance_valid(_anchor):
-				_link_distances.append(links[i].global_position.distance_to(_anchor.global_position))
+			if is_instance_valid(_pivot):
+				_link_distances.append(links[i].global_position.distance_to(_pivot.global_position))
 			else:
 				_link_distances.append(0.0)
 		else:
@@ -105,10 +105,7 @@ func _apply_constraint(delta: float) -> void:
 	if not _joint_valid:
 		return
 	
-	if is_instance_valid(_anchor):
-		_apply_constraint_with_anchor_on_velocity(delta)
-	else:
-		_apply_constraint_without_anchor_on_velocity(delta)
+	_apply_constraint_on_velocity(delta)
 
 
 func _apply_constraint_with_anchor_on_velocity(delta) -> void:
@@ -116,7 +113,7 @@ func _apply_constraint_with_anchor_on_velocity(delta) -> void:
 		var _next_pos: Vector2 = links[i].global_position + (links[i].linear_velocity * delta)
 		var _next_distance = 0.0
 		if i == 0:
-			_next_distance = _next_pos.distance_to(_anchor.global_position)
+			_next_distance = _next_pos.distance_to(_pivot.global_position)
 		else:
 			_next_distance = _next_pos.distance_to(links[i-1].global_position)
 		
@@ -125,11 +122,57 @@ func _apply_constraint_with_anchor_on_velocity(delta) -> void:
 		
 		var _v: Vector2 = Vector2.ZERO
 		if i == 0:
-			_v = _next_pos.direction_to(_anchor.global_position) * (_next_distance - _link_distances[i])
+			_v = _next_pos.direction_to(_pivot.global_position) * (_next_distance - _link_distances[i])
 		else:
 			_v = _next_pos.direction_to(links[i-1].global_position) * (_next_distance - _link_distances[i])
 		
 		links[i].linear_velocity += _v / delta
+
+
+func _apply_constraint_on_velocity(delta: float) -> void:
+	for i in links.size():
+		if i == 0 and is_instance_valid(_pivot):
+			var _next_pos: Vector2 = links[i].global_position + (links[i].linear_velocity * delta)
+			var _next_distance = 0.0
+			_next_distance = _next_pos.distance_to(_pivot.global_position)
+			
+			if _next_distance <= _link_distances[i]:
+				continue
+			
+			var _v: Vector2 = Vector2.ZERO
+			_v = _next_pos.direction_to(_pivot.global_position) * (_next_distance - _link_distances[i])
+			
+			links[i].linear_velocity += _v / delta
+		else:
+			var _next_pos = links[i].global_position + links[i].linear_velocity * delta
+			var _next_distance_previous = 0.0
+			var _next_distance_next = 0.0
+			
+			if i == 0:
+				_next_distance_next = _next_pos.distance_to(links[i + 1].global_position)
+			elif i == links.size() - 1:
+				_next_distance_previous = _next_pos.distance_to(links[i - 1].global_position)
+			else:
+				_next_distance_next = _next_pos.distance_to(links[i + 1].global_position)
+				_next_distance_previous = _next_pos.distance_to(links[i - 1].global_position)
+				
+			
+			if i < links.size() - 1:
+				if _next_distance_next > _link_distances[i + 1]:
+					var _distance = _link_distances[i + 1] - _next_distance_next
+					links[i].linear_velocity -= (
+						_next_pos.direction_to(links[i+1].global_position) *
+						_distance /
+						delta
+					)
+			if i > 0:
+				if _next_distance_previous > _link_distances[i]:
+					var _distance = _link_distances[i] - _next_distance_previous
+					links[i].linear_velocity -= (
+						_next_pos.direction_to(links[i-1].global_position) *
+						_distance /
+						delta
+					)
 
 
 func _apply_constraint_without_anchor_on_velocity(delta: float) -> void:
@@ -152,8 +195,7 @@ func _apply_constraint_without_anchor_on_velocity(delta: float) -> void:
 				var _distance = _link_distances[i + 1] - _next_distance_next
 				links[i].linear_velocity -= (
 					_next_pos.direction_to(links[i+1].global_position) *
-					(links[i+1].mass / links[i].mass) *
-					_distance *
+					_distance /
 					delta
 				)
 		if i > 0:
@@ -161,8 +203,7 @@ func _apply_constraint_without_anchor_on_velocity(delta: float) -> void:
 				var _distance = _link_distances[i] - _next_distance_previous
 				links[i].linear_velocity -= (
 					_next_pos.direction_to(links[i-1].global_position) *
-					(links[i-1].mass / links[i].mass) *
-					_distance *
+					_distance /
 					delta
 				)
 
